20fdbc012e96c2d7627748ff2914436e77b70fb5
Tales Henrique Bueno Rodrigues <tales.rodrigues@compasso.com.br>
2020-08-04 22:24:30 -0300

    Add project files.

---
+++ BackgroundTasksSample/Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace WebApiBackgroudServices.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok($"{DateTime.UtcNow:o}");
+        }
+    }
+}
---
+++ BackgroundTasksSample/Startup.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApiBackgroudServices
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
